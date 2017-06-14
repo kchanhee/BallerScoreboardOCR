952bd7e5b129a30c75bd35c5d21da84cc4073023
Chan Koh <koh.chanhee@gmail.com>
2017-06-13 17:28:38 -0700

    Initial Commit

---
+++ scoreboardOCR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scoreboardOCR
+//
+//  Created by Chan on 6/13/17.
+//  Copyright © 2017 Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scoreboardOCR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scoreboardOCR
+//
+//  Created by Chan on 6/13/17.
+//  Copyright © 2017 Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
